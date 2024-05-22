0933a2a55e58b3eb97f998eb7cb98fcb2f5c44a9
GHSoyad <ghsoyad@gmail.com>
2024-05-22 16:25:46 +0600

    Add container remove function

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ function getRandomColor() {
   return color;
 }
 
+
 function split(e) {
   const parent = e.target.parentNode.parentNode;
   const parentStyle = window.getComputedStyle(parent);
@@ -16,12 +17,14 @@ function split(e) {
 
   parent.innerHTML = `
   <div class='container' id='${parent.id}1' style='background-color: ${parentBackgroundColor};'>
+    <button class='remove' onclick='removeContainer(event)'>-</button>
     <div class='button-container'>
       <button onclick="split(event)">V</button>
       <button onclick="split(event)">H</button>
     </div>
   </div>
   <div class='container' id='${parent.id}2' style='background-color: ${newBackgroundColor};'>
+    <button class='remove' onclick='removeContainer(event)'>-</button>
     <div class='button-container'>
       <button onclick="split(event)">V</button>
       <button onclick="split(event)">H</button>
@@ -33,4 +36,28 @@ function split(e) {
   }
 }
 
-window.split = split;
+
+function removeContainer(e) {
+  const container = e.target.closest('.container');
+  container.remove();
+
+  const containers = document.querySelectorAll('.container');
+  // Check all containers in reverse and remove container without child
+  for (i = containers.length - 1; i >= 0; i--) {
+    const element = containers[i];
+    if (element.children.length === 0) {
+      element.remove();
+    }
+  }
+
+  // Reset to initial state after all containers are removed
+  if (document.querySelectorAll('.button-container').length === 0) {
+    document.body.innerHTML = `
+      <div class='container' id='1'>
+        <div class='button-container'>
+          <button onclick="split(event)">V</button>
+          <button onclick="split(event)">H</button>
+        </div>
+      </div>`
+  }
+}
