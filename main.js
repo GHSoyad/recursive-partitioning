50949dd993b0a29886bc86426e89fb4ca5a5a385
GHSoyad <ghsoyad@gmail.com>
2024-05-22 13:54:01 +0600

    Add random-color function

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 function split(e) {
   const parent = e.target.parentNode.parentNode;
+  const parentStyle = window.getComputedStyle(parent);
+
+  const parentBackgroundColor = parentStyle.backgroundColor;
+  const newBackgroundColor = getRandomColor();
 
   parent.innerHTML = `
-  <div class='container' id='${parent.id}1'>
+  <div class='container' id='${parent.id}1' style='background-color: ${parentBackgroundColor};'>
     <div class='button-container'>
       <button onclick="split(event)">V</button>
       <button onclick="split(event)">H</button>
     </div>
   </div>
-  <div class='container' id='${parent.id}2'>
+  <div class='container' id='${parent.id}2' style='background-color: ${newBackgroundColor};'>
     <div class='button-container'>
       <button onclick="split(event)">V</button>
       <button onclick="split(event)">H</button>
